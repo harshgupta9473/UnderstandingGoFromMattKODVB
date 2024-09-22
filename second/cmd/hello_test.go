4fc466ed70d574344d5986031518b530c0b4ea1b
harshgupta9473 <harshharsh9473@gmail.com>
2024-09-22 11:24:01 +0530

    some comments

--- second/cmd/hello_test.go
+++ second/cmd/hello_test.go
@@ -24,6 +24,9 @@ func TestSayHello(t *testing.T) {
 	for _,st:=range subtests{
 		if s:=Say(st.items); s!=st.result{
 			t.Errorf("wanted %s (%v), got %s",st.result,st.items,s)
+			//%v is a generic placeholder that prints the value in its default format. 
+			//It can be used with any type, 
+			//whether it's a string, integer, struct, slice, etc.
 		}
 	}
 
